fe080f303e022db4ca8c379dd562993e9f2cd9f5
Blazing-Falcon <farhanqpalm123@gmail.com>
2026-07-05 06:00:40 +0000

    chore: scaffold astro site and strapi cms

---
+++ site/astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [react()],
+
+  image: {
+    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '1337' }]
+  }
+});
